5bb36d73dc92b2ffe243bbadea3104a09d1e14a4
Bert Sinnema <bert@attachingit.com>
2017-10-18 13:07:35 +0200

    Updated comments

--- Exercise1.cs
+++ Exercise1.cs
@@ -4,11 +4,14 @@ namespace netcore_internship
 {
     public static class Exercise1
     {
+            // Count frequency of each element of an array
             public static void Run(){
                 
+                // For each value count the number of occurences. 
+                // Only output the occurence for a value once.
                 var arr = new int[]{ 6,6,9,8,5,4,7,8,5,3,2,3,2,3,6,5,8,5,8,6,9,6,9,9,5,8,7,4,7,8,4,5,9,5,2,6,5,8,7,1};
 
-                //Output: 1=1 2=3 3=3 4=3 5=8 6=6 7=4 8=7 9=5
+                // Your output should look like: 1=1 2=3 3=3 4=3 5=8 6=6 7=4 8=7 9=5
                 Console.WriteLine();
             }
     }
--- Exercise2.cs
+++ Exercise2.cs
@@ -4,11 +4,15 @@ namespace netcore_internship
 {
     public static class Exercise2
     {
+            // Sort an array of characters in alphabatic order
             public static void Run(){
                 
+                // Sort this array in alphabetical order and print the right order to the screen.        
                 var arr = new char[]{ 'z','q','a','u','w','r','c','f'};
+                // Your output should be: a c f q r u w z
 
-                //Output: a c f q r u w z
+
+                
                 Console.WriteLine();
             }
     }
