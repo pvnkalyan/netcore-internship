cd93a5b0b52f6b36ff402ea8e082da4e9bb47d9e
pvnkalyan <kalyanpvn025@gmail.com>
2017-10-24 17:56:16 +0530

    Update Exercise1.cs
    sovled using basic concepts.

--- Exercise1.cs
+++ Exercise1.cs
@@ -1,18 +1,27 @@
 using System;
-
+using System.Linq;
+using System.Collections.Generic;
+ 
 namespace netcore_internship
 {
-    public static class Exercise1
+    public class program
     {
-            // Count frequency of each element of an array
-            public static void Run(){
-                
-                // For each value count the number of occurences. 
-                // Only output the occurence for a value once.
-                var arr = new int[]{ 6,6,9,8,5,4,7,8,5,3,2,3,2,3,6,5,8,5,8,6,9,6,9,9,5,8,7,4,7,8,4,5,9,5,2,6,5,8,7,1};
-
-                // Your output should look like: 1=1 2=3 3=3 4=3 5=8 6=6 7=4 8=7 9=5
-                Console.WriteLine();
-            }
+        public void Run()
+        {
+             int[] arr1 = new int[] { 6,6,9,8,5,4,7,8,5,3,2,3,2,3,6,5,8,5,8,6,9,6,9,9,5,8,7,4,7,8,4,5,9,5,2,6,5,8,7,1};  
+             Array.Sort(arr1);	
+		     var n = from x in arr1  
+				group x by x into y  
+				select y;  
+				foreach (var arrNo in n)  
+				{  
+					Console.Write(arrNo.Key + " = " + arrNo.Count());  
+				} 
+    }
+        static void Main(String[] args)
+        {
+            program p=new program();
+            p.Run();
+        }
     }
 }
